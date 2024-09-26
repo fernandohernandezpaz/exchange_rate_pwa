f3711816df522a77e75444f6b9f0a417cb6b6620
fernandohernandezpaz <fernandohernandezpaz@outlook.com>
2024-09-26 11:35:10 -0600

    feat: implementing custom middleware capturing not found path

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,13 +1,16 @@
-import express from "express";
-import rateRoutes from "./infrastructure/express/routes/rate.routes";
+import express from 'express';
+import rateRoutes from './infrastructure/express/routes/rate.routes';
+import {notFoundMiddleware} from './infrastructure/express/middleware/notFoundMiddleware';
 
 const app = express();
 app.use(express.json());
+
 app.use('/api/rate', rateRoutes);
+app.use(notFoundMiddleware);
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`);
-})
+});
 
